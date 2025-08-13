73bc7e5e5f5e7adc50f0c5c34e7539287f4a5b0f
100ask <user@100ask.com>
2025-08-13 17:08:50 +0800

    01_hello_drv

--- hello_dev/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yochen0125/drv_practice/hello_dev/built-in.o :=  rm -f /home/yochen0125/drv_practice/hello_dev/built-in.o; arm-linux-gnueabihf-ar rcsD /home/yochen0125/drv_practice/hello_dev/built-in.o
--- hello_dev/.tmp_versions/hello_drv.mod
+++
@@ -1,3 +0,0 @@
-/home/yochen0125/drv_practice/hello_dev/hello_drv.ko
-/home/yochen0125/drv_practice/hello_dev/hello_drv.o
-
